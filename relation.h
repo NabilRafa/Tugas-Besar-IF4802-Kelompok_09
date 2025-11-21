27e76e6b27e234fe736f03e5e142bfbffd828181
NabilRafa <nabilr.athy@gmail.com>
2025-11-22 01:28:04 +0700

    Initial commit -CodeBlocksproject

---
+++ relation.h
@@ -0,0 +1,11 @@
+#ifndef RELATION_H_INCLUDED
+#define RELATION_H_INCLUDED
+
+#include "laundry.h"
+#include "customer.h"
+#include <iostream>
+
+using namespace std;
+
+
+#endif // RELATION_H_INCLUDED
