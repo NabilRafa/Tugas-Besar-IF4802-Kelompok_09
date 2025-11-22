f875f0a133180207661304c9091876ddcf4d4210
unknown <ajengwidjayanti@gmail.com>
2025-11-22 17:06:35 +0700

    Masukin ADT Customer (SLL) dan Laundry (DLL) (Insert & Delete)

--- customer.h
+++ customer.h
@@ -1,6 +1,32 @@
 #ifndef CUSTOMER_103012400144_H_INCLUDED
 #define CUSTOMER_103012400144_H_INCLUDED
 
+using namespace std;
+
+struct infotypeCustomer {
+    string nama, alamat;
+    int noTelp;
+};
+typedef struct elmList_Customer *addressC;
+
+struct elmList_Customer {
+    infotypeCustomer info;
+    addressC next;
+};
+
+struct listC {
+    addressC first;
+};
+
+void createListCustomer(listC &Lc);
+addressC createElmCustomer(listC Lc, infotypeCustomer x);
+void insertFirst_Customer(listC &Lc, addressC P);
+void insertLast_Customer(listC &Lc, addressC P);
+void insertAfter_Customer(listC &Lc, addressC prec, addressC P);
+void deleteFirst_Customer(listC &Lc, addressC &P);
+void deleteLast_Customer(listC &Lc, addressC &P);
+void deleteAfter_Customer(listC &Lc, addressC prec, addressC &P);
+
 
 
 #endif // CUSTOMER_103012400144_H_INCLUDED
--- laundry.h
+++ laundry.h
@@ -1,6 +1,28 @@
 #ifndef LAUNDRY_103012400112_H_INCLUDED
 #define LAUNDRY_103012400112_H_INCLUDED
 
+using namespace std;
 
+struct infotypePakaian {
+    string jenis, layanan, status;
+};
+typedef struct elmList_Pakaian *addressP;
+
+struct elmList_Pakaian {
+    infotypePakaian info;
+    addressP next, prev;
+};
+
+struct listP {
+    addressP first, last;
+};
+
+addressP createElmPakaian(listP Lp, infotypePakaian x);
+void insertFirst_Pakaian(listP &Lp, addressP P);
+void insertLast_Pakaian(listP &Lp, addressP P);
+void insertAfter_Pakaian(listP &Lp, addressP prec, addressP P);
+void deleteFirst_Pakaian(listP &Lp, addressP &P);
+void deleteLast_Pakaian(listP &Lp, addressP &P);
+void deleteAfter_Pakaian(listP &Lp, addressP prec, addressP &P);
 
 #endif // LAUNDRY_103012400112_H_INCLUDED
