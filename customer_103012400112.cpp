a020684447f8d6004c8ae54f11c9f77386ddb878
unknown <ajengwidjayanti@gmail.com>
2025-12-06 07:10:53 +0700

    Prosedur Delete

--- customer_103012400112.cpp
+++ customer_103012400112.cpp
@@ -3,6 +3,37 @@
 
 using namespace std;
 
-void deleteFirst_Customer(ListCustomer &LC, addressCustomer &P);
-void deleteLast_Customer(ListCustomer &LC, addressCustomer &P);
-void deleteAfter_Customer(ListCustomer &LC, addressCustomer prec, addressCustomer &P);
+void deleteFirst_Customer(ListCustomer &LC, addressCustomer &P) {
+    if (LC.first == nullptr) {
+        cout << "Tidak ada data yang bisa dihapus";
+        P = nullptr;
+    } else {
+        P = LC.first;
+        LC.first = P->next;
+        P->next = nullptr;
+    }
+}
+void deleteLast_Customer(ListCustomer &LC, addressCustomer &P) {
+    addressCustomer Q;
+    if (LC.first == nullptr) {
+        cout << "Tidak ada data yang bisa dihapus"
+        P = nullptr;
+    } else {
+        Q = LC.first;
+        while (Q->next->next != nullptr) {
+            Q = Q->next;
+        }
+        P = Q->next;
+        Q->next = nullptr;
+    }
+}
+void deleteAfter_Customer(ListCustomer &LC, addressCustomer prec, addressCustomer &P) {
+    if (LC.first == nullptr || prec->next == nullptr) {
+        cout << "Tidak ada data yang bisa dihapus"
+        P = nullptr;
+    } else {
+        P = prec->next;
+        prec->next = P->next;
+        P->next = nullptr;
+    }
+}
--- laundry_103012400112.cpp
+++ laundry_103012400112.cpp
@@ -3,6 +3,33 @@
 
 using namespace std;
 
-void deleteFirst_Pakaian(ListLaundry &LL, addressLaundry &P);
-void deleteLast_Pakaian(ListLaundry &LL, addressLaundry &P);
-void deleteAfter_Pakaian(ListLaundry &LL, addressLaundry prec, addressLaundry &P);
+void deleteFirst_Pakaian(ListLaundry &LL, addressLaundry &P) {
+    if (LL.first == nullptr && LL.last == nullptr) {
+        cout << "Tidak ada data yang bisa dihapus"
+        P = nullptr;
+    } else {
+        P = LL.first;
+        LL.first = P->next;
+        P->next = nullptr;
+    }
+}
+void deleteLast_Pakaian(ListLaundry &LL, addressLaundry &P) {
+    if (LL.first == nullptr && LL.last == nullptr) {
+        cout << "Tidak ada data yang bisa dihapus"
+        P = nullptr;
+    } else {
+        P = LL.last;
+        LL.last = P->prev;
+        P->prev->next = nullptr;
+    }
+}
+void deleteAfter_Pakaian(ListLaundry &LL, addressLaundry prec, addressLaundry &P) {
+    if (LL.first == nullptr && LL.last == nullptr || prec->next == nullptr) {
+        cout << "Tidak ada data yang bisa dihapus"
+        P = nullptr;
+    } else {
+        P = prec->next;
+        prec->next = P->next;
+        P->next = nullptr;
+    }
+}
